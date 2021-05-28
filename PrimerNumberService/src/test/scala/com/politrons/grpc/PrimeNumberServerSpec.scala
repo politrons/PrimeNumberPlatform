33df1a451ee0badb5dac35b08895271c4fe53600
politrons <politrons@gmail.com>
2021-05-28 20:34:36 +0200

    Add Unit test for PrimeNumberServer and fix maven issue that was not running the test in maven lifecycle

--- PrimerNumberService/src/test/scala/com/politrons/grpc/PrimeNumberServerSpec.scala
+++ PrimerNumberService/src/test/scala/com/politrons/grpc/PrimeNumberServerSpec.scala
@@ -1,40 +1,91 @@
 package com.politrons.grpc
 
+import io.grpc.ManagedChannelBuilder
 import io.grpc.stub.StreamObserver
 import org.scalatest.{BeforeAndAfterAll, FeatureSpec, GivenWhenThen}
+import zio.{Has, Runtime, ZIO, ZLayer}
 
 import scala.concurrent.duration._
-import scala.concurrent.{Await, Promise}
+import scala.concurrent.{Await, ExecutionContextExecutor, Future, Promise}
 import scala.language.postfixOps
 
-//TODO:Think about doing it better in a IT test
-//class PrimeNumberServerSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfterAll {
-//
-//  val promise: Promise[String] = Promise()
-//  feature("PrimeNumberServer to check that server run as we expect ") {
-//    scenario("I start the PrimeNumberServer and I connect with a client") {
-//      Given("A server running")
-//      val service = new PrimeNumberServiceImpl()
-//      When("I invoke to get findPrimeNumbers ")
-//      val observer: StreamObserver[PrimeNumberRequest] = service.findPrimeNumbers(new StreamObserver[PrimeNumberResponse]() {
-//        override def onNext(value: PrimeNumberResponse): Unit = {
-//          promise.success(value.getValue)
-//        }
-//
-//        override def onError(t: Throwable): Unit = {
-//          promise.failure(t)
-//        }
-//
-//        override def onCompleted(): Unit = {
-//          System.out.println("Stream finish")
-//        }
-//      })
-//      val primeNumber = "17"
-//      val request: PrimeNumberRequest = PrimeNumberRequest.newBuilder.setAttr(primeNumber).build;
-//      observer.onNext(request)
-//      Then("it return the expected number of prime numbers")
-//      assert(Await.result(promise.future, 30 seconds) == primeNumber)
-//    }
-//  }
-//
-//}
+class PrimeNumberServerSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfterAll {
+
+  implicit val ec: ExecutionContextExecutor = scala.concurrent.ExecutionContext.global
+
+  val promise: Promise[String] = Promise()
+  feature("PrimeNumberServer to check that server run as we expect ") {
+    scenario("I start the PrimeNumberServer, passing a mock service, " +
+      "and then I connect with a mock client") {
+      Given("A server program")
+      val serverProgram: ZIO[Has[PrimeNumberServiceGrpc.PrimeNumberServiceImplBase], Throwable, Unit] =
+        PrimerNumberServer.start()
+      When("I evaluate the server program ")
+      val serviceMock: PrimeNumberServiceGrpc.PrimeNumberServiceImplBase = PrimeNumberServiceMock()
+      Future {
+        Runtime.global.unsafeRun(serverProgram.provideLayer(ZLayer.succeed(serviceMock)))
+      }
+
+      And("I make request to the server ")
+      //TODO:I know I know! This is really bad!
+      Thread.sleep(5000)
+      val primeNumber = "17"
+      PrimeNumberServerSpec.runRequest(primeNumber)
+      Then("it return the expected number of prime numbers")
+      assert(Await.result(promise.future, 30 seconds) == primeNumber)
+    }
+  }
+
+  case class PrimeNumberServiceMock() extends PrimeNumberServiceGrpc.PrimeNumberServiceImplBase {
+
+    override def findPrimeNumbers(responseObserver: StreamObserver[PrimeNumberResponse]): StreamObserver[PrimeNumberRequest] = {
+      new StreamObserver[PrimeNumberRequest]() {
+        override def onNext(value: PrimeNumberRequest): Unit = {
+          System.out.println("[PrimeNumberServiceMock] Prime number from client: " + value.getAttr)
+          promise.success(value.getAttr)
+        }
+
+        override def onError(t: Throwable): Unit = {
+          promise.failure(t)
+        }
+
+        override def onCompleted(): Unit = {
+        }
+      }
+    }
+
+  }
+}
+
+object PrimeNumberServerSpec {
+
+  def runRequest(primeNumber: String): Unit = {
+
+    val channel = ManagedChannelBuilder
+      .forAddress("localhost", 9995)
+      .usePlaintext(true).asInstanceOf[ManagedChannelBuilder[_]]
+      .build()
+
+    val stub: PrimeNumberServiceGrpc.PrimeNumberServiceStub = PrimeNumberServiceGrpc.newStub(channel)
+
+    val request: PrimeNumberRequest = PrimeNumberRequest.newBuilder.setAttr(primeNumber).build
+    val stream: StreamObserver[PrimeNumberRequest] =
+      stub.findPrimeNumbers(new StreamObserver[PrimeNumberResponse]() {
+        override def onNext(response: PrimeNumberResponse): Unit = {
+          System.out.println(s"Prime number mock client Stream on next ${response.getValue}")
+        }
+
+        override def onError(t: Throwable): Unit = {
+          throw t
+        }
+
+        override def onCompleted(): Unit = {
+          System.out.println("Prime number mock client Stream finish")
+        }
+      })
+
+    stream.onNext(request)
+  }
+}
+
+
