cf807862c0f9da138e4a8bc864eac86b1d4f2c73
politrons <politrons@gmail.com>
2021-05-31 10:25:57 +0200

    reduce traffic of performance test to avoid too much noise in the logs

--- test-framework/src/test/scala/com/politrons/performance/simulation/PrimeNumberPlatformSimulation.scala
+++ test-framework/src/test/scala/com/politrons/performance/simulation/PrimeNumberPlatformSimulation.scala
@@ -9,16 +9,20 @@ import io.gatling.core.structure.ScenarioBuilder
 import scala.concurrent.duration._
 import scala.concurrent.duration.{Duration, SECONDS}
 
+/**
+ * TODO:WE have to upgrade Gatling to one of the latest version to allow http2 and avoid close the connection
+ * For now I will drastically reduce the traffic to avoid too much noise in the logs.
+ */
 class PrimeNumberPlatformSimulation extends Simulation {
 
   val idleTime = 1
-  val users = 1000
-  val time = 60
+  val users = 10
+  val time = 10
 
   private val primeNumberScn: ScenarioBuilder = new PrimeNumberPlatformScene().create()
 
   setUp(
-    primeNumberScn.inject(nothingFor(idleTime.seconds), rampUsers(users) over Duration(60, SECONDS)))
+    primeNumberScn.inject(nothingFor(idleTime.seconds), rampUsers(users) over Duration(time, SECONDS)))
     .protocols(GatlingHttpClient.conf)
     .assertions(global.responseTime.max.lessThan(5000))
     .assertions(global.responseTime.mean.lessThan(100))
